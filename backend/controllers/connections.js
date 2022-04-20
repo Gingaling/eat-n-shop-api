16d4c3d16e79d67aa227e7358a91e4ef26e30618
Gingaling <nycgingsf@hotmail.com>
2022-04-20 01:35:54 -0700

    Committing files to new kitchen-counter-api.

---
+++ backend/controllers/connections.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.DATABASE_URL;
+
+const db = mongoose.connection;
+
+mongoose.connect(mongoURI);
+
+db.on('error', err => console.log(err.message + ' is MongoDB not running?'));
+db.on('connected', () => console.log('MongoDB connected at: ', mongoURI));
+db.on('disconnected', () => console.log('MongoDB disconnected'));
+
+db.on('open', () => {
+	console.log('✅ MongoDB connection made!🧜‍♀️');
+});
+
+module.exports = mongoose;
---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const groceryController = require('./controllers/groceryController');
+
+app.set('port', process.env.PORT || 8000);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.redirect('/grocery');
+});
+
+app.use('/grocery', groceryController);
+
+app.listen(app.get('port'), () => {
+	console.log(`✅ PORT: ${app.get('port')} 🌟`);
+});
